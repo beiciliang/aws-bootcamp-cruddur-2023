126dab380a1e29c89d100089e87bb7a2dc4e2498
Beici Liang <beici.liang@foxmail.com>
2023-02-28 11:05:40 +0000

    implement rollbar

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -32,6 +32,11 @@ import watchtower
 import logging
 from time import strftime
 
+# rollbar
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
 # # Configuring Logger to Use CloudWatch
 # # Comment for saving AWS budget
 # LOGGER = logging.getLogger(__name__)
@@ -52,9 +57,9 @@ from time import strftime
 provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
-# Show this in the logs within the backend-flask app (STDOUT)
-simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
-provider.add_span_processor(simple_processor)
+# # Show this in the logs within the backend-flask app (STDOUT)
+# simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
+# provider.add_span_processor(simple_processor)
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
@@ -84,6 +89,29 @@ cors = CORS(
 #     timestamp = strftime('[%Y-%b-%d %H:%M]')
 #     LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
 #     return response
+
+# rollbar
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
