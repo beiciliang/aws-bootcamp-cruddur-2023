ff1b9254ee2c9250f053ff17e434815a1ac5ef6b
Beici Liang <beici.liang@foxmail.com>
2023-03-13 13:07:53 +0000

    replace mock endpoint with real api call

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,9 +1,41 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
+from lib.db import pool, query_wrap_array
 
 tracer = trace.get_tracer("home.activities")
 
+class HomeActivities:
+    def run(cognito_user_id=None):
+        sql = query_wrap_array("""
+        SELECT
+            activities.uuid,
+            users.display_name,
+            users.handle,
+            activities.message,
+            activities.replies_count,
+            activities.reposts_count,
+            activities.likes_count,
+            activities.reply_to_activity_uuid,
+            activities.expires_at,
+            activities.created_at
+        FROM public.activities
+        LEFT JOIN public.users ON users.uuid = activities.user_uuid
+        ORDER BY activities.created_at DESC
+        """)
+        print("SQL--------------")
+        print(sql)
+        print("SQL--------------")
+        with pool.connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(sql)
+                # this will return a tuple
+                # the first field being the data
+                json = cur.fetchone()
+        print(json[0])
+        return json[0]
 
+"""
+# mock endpoint
 class HomeActivities:
     def run(cognito_user_id=None):
         # def run(logger):
@@ -66,3 +98,4 @@ class HomeActivities:
 
             span.set_attribute("app.result_length", len(results))
             return results
+"""
