2080b2affdfd1efa3dd471f7f8b2845b1faf47fd
Beici Liang <beici.liang@deusvault.co.uk>
2023-03-14 14:38:08 +0100

    refactor the db library and create activity

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,38 +1,17 @@
 from datetime import datetime, timedelta, timezone
-from opentelemetry import trace
-from lib.db import pool, query_wrap_array
+from lib.db import db
+# from opentelemetry import trace
+# from lib.db import pool, query_wrap_array
 
-tracer = trace.get_tracer("home.activities")
+# tracer = trace.get_tracer("home.activities")
+
 
 class HomeActivities:
     def run(cognito_user_id=None):
-        sql = query_wrap_array("""
-        SELECT
-            activities.uuid,
-            users.display_name,
-            users.handle,
-            activities.message,
-            activities.replies_count,
-            activities.reposts_count,
-            activities.likes_count,
-            activities.reply_to_activity_uuid,
-            activities.expires_at,
-            activities.created_at
-        FROM public.activities
-        LEFT JOIN public.users ON users.uuid = activities.user_uuid
-        ORDER BY activities.created_at DESC
-        """)
-        print("SQL--------------")
-        print(sql)
-        print("SQL--------------")
-        with pool.connection() as conn:
-            with conn.cursor() as cur:
-                cur.execute(sql)
-                # this will return a tuple
-                # the first field being the data
-                json = cur.fetchone()
-        print(json[0])
-        return json[0]
+        sql = db.template('activities', 'home')
+        results = db.query_array_json(sql)
+        return results
+
 
 """
 # mock endpoint
