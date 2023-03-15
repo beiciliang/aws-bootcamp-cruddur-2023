67c2a277af11edbe4061e6b9e6f136afe1424334
Beici Liang <beici.liang@foxmail.com>
2023-03-15 11:39:55 +0000

    temp fix of uuid none error

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -216,7 +216,7 @@ def data_search():
 @app.route("/api/activities", methods=['POST', 'OPTIONS'])
 @cross_origin()
 def data_activities():
-    user_handle = 'andrewbrown'
+    user_handle = 'beiciliang'
     message = request.json['message']
     ttl = request.json['ttl']
     model = CreateActivity.run(message, user_handle, ttl)
