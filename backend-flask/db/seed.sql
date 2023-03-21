d9405cfc2ee5217fbec5798ee07115eab4368229
Beici Liang <beici.liang@deusvault.co.uk>
2023-03-21 13:27:38 +0100

    add DynamoDB utility scripts

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,8 +1,9 @@
 -- this file was manually created
-INSERT INTO public.users (display_name, handle, cognito_user_id)
+INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrewbrown' ,'MOCK'),
-  ('Beici Liang', 'beiciliang' ,'MOCK');
+  ('Andrew Brown', 'andrew@exampro.co', 'andrewbrown' ,'MOCK'),
+  ('Andrew Bayko','bayko@exampro.co' , 'bayko' ,'MOCK'),
+  ('Beici Liang', 'liangbeici@gmail.com', 'beiciliang' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -55,11 +55,11 @@ class Db:
         for key, value in params.items():
             print(key, ":", value)
 
-    def print_sql(self, title, sql):
+    def print_sql(self, title, sql, params={}):
         cyan = '\033[96m'
         no_color = '\033[0m'
         print(f'{cyan} SQL STATEMENT-[{title}]------{no_color}')
-        print(sql)
+        print(sql, params)
 
     # we want to commit data such as an insert
     # be sure to check for RETURNING in all uppercases
@@ -83,7 +83,7 @@ class Db:
 
     # when we want to return a json object
     def query_array_json(self, sql, params={}):
-        self.print_sql('array', sql)
+        self.print_sql('array', sql, params)
 
         wrapped_sql = self.query_wrap_array(sql)
         with self.pool.connection() as conn:
@@ -95,7 +95,7 @@ class Db:
     # when we want to return an array of json objects
     def query_object_json(self, sql, params={}):
 
-        self.print_sql('json', sql)
+        self.print_sql('json', sql, params)
         self.print_params(params)
         wrapped_sql = self.query_wrap_object(sql)
 
@@ -107,6 +107,14 @@ class Db:
                     "{}"
                 else:
                     return json[0]
+
+    def query_value(self, sql, params={}):
+        self.print_sql('value', sql, params)
+        with self.pool.connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(sql, params)
+                json = cur.fetchone()
+                return json[0]
 
     def query_wrap_object(self, template):
         sql = f"""
