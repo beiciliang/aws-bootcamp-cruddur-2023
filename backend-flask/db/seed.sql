7392ad79a0c4151af859477b589539fd321dbe60
Beici Liang <beici.liang@foxmail.com>
2023-03-22 19:05:44 +0000

    change handle from andrewbrown to beiciliang

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,14 +1,14 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrew@exampro.co', 'andrewbrown' ,'MOCK'),
+  ('Beici Liang', 'liangbeici@gmail.com', 'beiciliang' ,'MOCK'),
   ('Andrew Bayko','bayko@exampro.co' , 'bayko' ,'MOCK'),
-  ('Beici Liang', 'liangbeici@gmail.com', 'beiciliang' ,'MOCK');
+  ('Londo Mollari','lmollari@centari.com' ,'londo' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'beiciliang' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
