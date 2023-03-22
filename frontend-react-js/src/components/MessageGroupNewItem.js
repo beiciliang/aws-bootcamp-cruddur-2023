1bc5aef1d74b131fe7e2115c5e537f1d66f84942
Beici Liang <beici.liang@deusvault.co.uk>
2023-03-22 19:16:36 +0100

    fix a typo

--- frontend-react-js/src/components/MessageGroupNewItem.js
+++ frontend-react-js/src/components/MessageGroupNewItem.js
@@ -7,7 +7,7 @@ export default function MessageGroupNewItem(props) {
     <Link className='message_group_item active' to={`/messages/new/`+props.user.handle}>
       <div className='message_group_avatar'></div>
       <div className='message_content'>
-        <div classsName='message_group_meta'>
+        <div className='message_group_meta'>
           <div className='message_group_identity'>
             <div className='display_name'>{props.user.display_name}</div>
             <div className="handle">@{props.user.handle}</div>
