294af409aae98f50c32f48ebc244d21c826434f6
Beici Liang <beici.liang@deusvault.co.uk>
2023-03-22 17:36:58 +0100

    implement conversations with DynamoDB

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -64,7 +64,7 @@ class Db:
     # we want to commit data such as an insert
     # be sure to check for RETURNING in all uppercases
     def query_commit(self, sql, params={}):
-        self.print_sql('commit with returning', sql)
+        self.print_sql('commit with returning', sql, params)
 
         pattern = r"\bRETURNING\b"
         is_returning_id = re.search(pattern, sql)
