37ce660e8fb72518bd97fa747bb3f71437f18eff
Beici Liang <beici.liang@foxmail.com>
2023-04-03 14:39:38 +0000

    add script and route for backend health check

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -121,10 +121,15 @@ def init_rollbar():
     got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
 
-@app.route('/rollbar/test')
-def rollbar_test():
-    rollbar.report_message('Hello World!', 'warning')
-    return "Hello World!"
+# @app.route('/rollbar/test')
+# def rollbar_test():
+#     rollbar.report_message('Hello World!', 'warning')
+#     return "Hello World!"
+
+
+@app.route('/api/health-check')
+def health_check():
+    return {'success': True}, 200
 
 
 @app.route("/api/message_groups", methods=['GET'])
