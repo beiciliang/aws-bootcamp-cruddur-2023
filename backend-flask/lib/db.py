fce0bd577a919e28a69e60da20940d93c0b7c03b
Beici Liang <beici.liang@deusvault.co.uk>
2023-04-04 00:25:42 +0200

    update backend dockerfile for dev and prod

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -104,7 +104,7 @@ class Db:
                 cur.execute(wrapped_sql, params)
                 json = cur.fetchone()
                 if json == None:
-                    "{}"
+                    return "{}"
                 else:
                     return json[0]
 
